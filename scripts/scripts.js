5d7ce3547b67e4340cf67b30966f8c5a70543ed7
Allan Montilla <allan.montilla@herodigital.com>
2024-07-05 16:34:18 -0500

    Add new styles for blog breadcrumb

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,6 +11,7 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
+  getMetadata,
 } from './aem.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
@@ -57,21 +58,71 @@ function buildAutoBlocks(main) {
   }
 }
 
+function getBlogBaseUrl(url) {
+  try {
+    const urlObj = new URL(url);
+    const pathParts = urlObj.pathname.split('/');
+
+    if (pathParts.length > 2 && pathParts[2] === 'blog') {
+      urlObj.pathname = `/${pathParts[1]}/blog`;
+    } else if (pathParts.length > 1 && pathParts[1] === 'blog') {
+      urlObj.pathname = '/blog';
+    } else {
+      throw new Error('URL does not match expected pattern');
+    }
+
+    return urlObj.toString();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Invalid URL:', error);
+    return null;
+  }
+}
+
 async function decorateBlog(main) {
   if (main.parentElement && main.parentElement.matches('body[class="blog-page"]')) {
-    // console.log('Blog page detected');
+    const blogBreadcrumb = getMetadata('blog-breadcrumb');
+    const { title } = document;
     const leftColumn = document.createElement('div');
     leftColumn.classList.add('blog-content');
     const rightColumn = document.createElement('div');
     rightColumn.classList.add('related-content');
+    // Create related content wrapper
+    const rcWrapper = document.createElement('div');
+    rcWrapper.classList.add('related-content-wrapper');
+
     const rcHeader = document.createElement('h4');
     rcHeader.classList.add('related-content-header');
-    rightColumn.append(rcHeader);
-
+    rcWrapper.append(rcHeader);
+    const mainSection = main.querySelector('div.section');
     const defaultContent = main.querySelector('div.section > div.default-content-wrapper');
     leftColumn.append(...defaultContent.childNodes);
+    rightColumn.append(rcWrapper);
     defaultContent.prepend(rightColumn);
     defaultContent.prepend(leftColumn);
+
+    // Create title breadcrumb
+    const titleBreadcrumb = document.createElement('span');
+    titleBreadcrumb.classList.add('title-breadcrumb');
+    titleBreadcrumb.textContent = title;
+
+    // Create blog breadcrumb with a link
+    const blogBreadcrumbElement = document.createElement('p');
+    blogBreadcrumbElement.classList.add('blog-breadcrumb');
+    // Get the blog base URL
+    const blogBaseUrl = getBlogBaseUrl(window.location.href);
+    const blogLinkElement = document.createElement('a');
+    blogLinkElement.textContent = blogBreadcrumb;
+    blogLinkElement.href = blogBaseUrl;
+    blogLinkElement.setAttribute('aria-label', blogBreadcrumb);
+    blogBreadcrumbElement.append(blogLinkElement, titleBreadcrumb);
+
+    // Create a wrapper div for breadcrumbs
+    const breadcrumbWrapper = document.createElement('div');
+    breadcrumbWrapper.classList.add('breadcrumb-wrapper');
+    breadcrumbWrapper.append(blogBreadcrumbElement);
+    // add the breadcrumbWrapper to the start of the leftColumn
+    mainSection.prepend(breadcrumbWrapper);
   }
 }
 
