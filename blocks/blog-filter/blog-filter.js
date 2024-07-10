5b67c006a6558e2cd6ae8a425add4f60d9f44379
Darin Kuntze <dkuntze@gmail.com>
2024-07-10 13:23:32 -0500

    wip for facets

--- blocks/blog-filter/blog-filter.js
+++ blocks/blog-filter/blog-filter.js
@@ -3,25 +3,72 @@ import { getDateFromExcel } from '../../scripts/scripts.js';
 
 async function getResults() {
   const postArray = [];
-  const posts = await ffetch('/query-index.json').sheet('blog').all();
-
+  const posts = await ffetch('/query-index.json').sheet('blog')
+    .limit(10).all();
   posts.forEach((post) => {
     postArray.push(post);
   });
   return postArray;
 }
 
-const formatDate = (date) => {
-  return date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit'
+async function getFacets() {
+  const facetArray = [];
+  const facets = await ffetch('/query-index.json')
+    .sheet('blog')
+    .all();
+  const tagJson = {};
+  facets.forEach((facet) => {
+    const tags = facet.tags.split(',');
+    tags.forEach((tag) => {
+      const cleanTag = tag.trim().replaceAll(/["\[\]]/g, '');
+      // toss out blog tags and empty ones
+      if (cleanTag === 'Blogs' || cleanTag === '') {
+        return;
+      }
+      const existingTag = facetArray.find((e) => e.tag === cleanTag);
+      if (existingTag) {
+        existingTag.count += 1;
+      } else {
+        facetArray.push({ tag: cleanTag, count: 1 });
+      }
+    });
   });
-};
+  tagJson.tags = facetArray;
+  return tagJson;
+}
+
+const formatDate = (date) => date.toLocaleDateString('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: '2-digit',
+});
 
 export default async function decorate(block) {
+  const facets = await getFacets();
   const facetDiv = document.createElement('div');
   facetDiv.classList.add('facet');
+  const facetList = document.createElement('ul');
+  const facetLI = document.createElement('li');
+  const topicHead = document.createElement('div');
+  topicHead.innerText = 'TOPIC';
+  facetLI.append(topicHead);
+  facetList.append(facetLI);
+  const facetUL = document.createElement('ul');
+  facetList.append(facetUL);
+  facets.tags.forEach((facet) => {
+    const facetItem = document.createElement('li');
+    const topic = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const span = document.createElement('span');
+    span.innerText = `${facet.tag} (${facet.count})`;
+    checkbox.type = 'checkbox';
+    checkbox.value = facet.tag;
+    topic.append(checkbox);
+    topic.append(span);
+    facetItem.append(topic);
+    facetUL.append(facetItem);
+  });
+  facetDiv.append(facetList);
   const resultsDiv = document.createElement('div');
   resultsDiv.classList.add('results');
   const resultsList = document.createElement('ul');
