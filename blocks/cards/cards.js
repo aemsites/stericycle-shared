94a573ad806798945b46dde638ad91251aa4fc99
Luca <adobe@gituser.de>
2024-07-30 11:37:53 +0200

    add card style

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,48 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  const cardList = document.createElement('ul');
+
+  // populate cards
+  let rowsWithContentLeft = true;
+  while (rowsWithContentLeft) {
+    rowsWithContentLeft = false;
+
+    const card = document.createElement('li');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    let isFirstSection = true;
+    const rows = [...block.children];
+    for (let i = 0; i < rows.length; i += 1) {
+      const cardSection = rows[i].firstElementChild;
+      if (cardSection) {
+        if (cardSection.nextElementSibling) {
+          rowsWithContentLeft = true;
+        }
+
+        // populate card
+        cardSection.classList.add('card-section');
+        cardBody.append(cardSection);
+
+        if (isFirstSection) {
+          // create card-head
+          if (cardSection.firstElementChild?.querySelector('span.icon')) {
+            const cardHead = document.createElement('div');
+            cardHead.classList.add('card-head');
+            cardHead.append(cardSection);
+            card.append(cardHead);
+          } else {
+            isFirstSection = false;
+          }
+        } else if (block.classList.contains('divider')) {
+          // add divider
+          const divider = document.createElement('hr');
+          cardSection.insertAdjacentElement('beforebegin', divider);
+        }
+      }
+    }
+    card.append(cardBody);
+    cardList.append(card);
+  }
+
   block.textContent = '';
-  block.append(ul);
+  block.append(cardList);
 }
