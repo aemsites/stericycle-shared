e9d4241bd7e03d5a41a034f1f5026ef9f98cd7dd
Luca <adobe@gituser.de>
2024-08-28 13:53:12 +0200

    mitigate CLS

--- blocks/post-teaser-list/post-teaser-list.js
+++ blocks/post-teaser-list/post-teaser-list.js
@@ -3,9 +3,58 @@ import { createOptimizedPicture, decorateButtons, readBlockConfig } from '../../
 
 function createPostLink(post) {
   const anchor = document.createElement('a');
-  anchor.setAttribute('aria-label', post.title);
-  anchor.href = post.path;
+  if (post) {
+    anchor.setAttribute('aria-label', post.title);
+    anchor.href = post.path;
+  }
   return anchor;
+}
+
+function addPost(list, post, isLoading) {
+  // create teaser
+  const teaser = document.createElement('li');
+  teaser.classList.add('teaser');
+  if (isLoading) {
+    teaser.classList.add('loading');
+  }
+  list.append(teaser);
+
+  // create thumbnail
+  const thumbnailLink = createPostLink(post);
+  teaser.append(thumbnailLink);
+  let thumbnail;
+  if (isLoading) {
+    thumbnail = document.createElement('div');
+  } else {
+    thumbnail = createOptimizedPicture(post.image, post.title);
+  }
+  thumbnail.classList.add('teaser-thumbnail');
+  thumbnailLink.append(thumbnail);
+
+  // create content
+  const content = document.createElement('div');
+  content.classList.add('teaser-content');
+  teaser.append(content);
+  if (isLoading) {
+    return;
+  }
+  const title = createPostLink(post);
+  title.classList.add('teaser-title');
+  title.textContent = post?.title;
+  content.append(title);
+  const category = document.createElement('a');
+  category.classList.add('teaser-category');
+  const mediaType = post['media-type'];
+  category.setAttribute('aria-label', mediaType);
+  category.href = '#'; // TODO: category url from index
+  category.textContent = mediaType;
+  content.append(category);
+  const buttonWrapper = document.createElement('p');
+  content.append(buttonWrapper);
+  const button = createPostLink(post);
+  button.textContent = 'Read More';
+  buttonWrapper.append(button);
+  decorateButtons(buttonWrapper);
 }
 
 export default function decorate(block) {
@@ -15,43 +64,21 @@ export default function decorate(block) {
     columnCount = 4;
   }
   block.innerHTML = '';
+
+  // create wrapper
+  const list = document.createElement('ul');
+  list.classList.add('teaser-list');
+  block.append(list);
+
+  // populate with dummies while loading
+  for (let i = 0; i < columnCount; i += 1) {
+    addPost(list, null, true);
+  }
+
   getRelatedPosts((type || '').split(/,\s*]/), type, columnCount).then((posts) => {
-    const list = document.createElement('ul');
-    list.classList.add('teaser-list');
+    list.innerHTML = '';
     posts.forEach((post) => {
-      // create teaser
-      const teaser = document.createElement('li');
-      teaser.classList.add('teaser');
-      list.append(teaser);
-
-      // create thumbnail
-      const thumbnailLink = createPostLink(post);
-      teaser.append(thumbnailLink);
-      const thumbnail = createOptimizedPicture(post.image, post.title);
-      thumbnail.classList.add('teaser-thumbnail');
-      thumbnailLink.append(thumbnail);
-
-      // create content
-      const content = document.createElement('div');
-      content.classList.add('teaser-content');
-      teaser.append(content);
-      const title = createPostLink(post);
-      title.classList.add('teaser-title');
-      title.textContent = post.title;
-      content.append(title);
-      const category = document.createElement('a');
-      category.classList.add('teaser-category');
-      category.setAttribute('aria-label', post['media-type']);
-      category.href = '#'; // TODO: category url from index
-      category.textContent = post['media-type'];
-      content.append(category);
-      const buttonWrapper = document.createElement('p');
-      content.append(buttonWrapper);
-      const button = createPostLink(post);
-      button.textContent = 'Read More';
-      buttonWrapper.append(button);
-      decorateButtons(buttonWrapper);
+      addPost(list, post, false);
     });
-    block.append(list);
   });
 }
