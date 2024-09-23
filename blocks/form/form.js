aaa3b723e475fb32cd4d9aedcf5aba8e77dd2c2c
vdua <vdua@adobe.com>
2024-09-23 11:27:04 +0530

    feat: support for floating labels

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -6,6 +6,20 @@ export default async function decorate(block) {
   const { container, formDef } = await extractSheetDefinition(block);
   if (formDef && formDef.properties?.source === 'sheet') {
     const form = await renderDocForm(formDef);
-    container.replaceWith(form);
+    if (form) {
+      container.replaceWith(form);
+      const inputs = form.querySelectorAll('.field-wrapper input');
+      const excludeTypes = ['checkbox', 'radio'];
+      inputs.forEach((input) => {
+        if (!excludeTypes.includes(input.type)) {
+          const wrapper = input.closest('.field-wrapper');
+          wrapper.insertAdjacentElement('afterbegin', input);
+          wrapper.classList.add('floating-field');
+          if (!input.placeholder) {
+            input.placeholder = '';
+          }
+        }
+      });
+    }
   }
 }
