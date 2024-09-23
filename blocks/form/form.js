6222f3b61d58334e136f5cce225310b4017d8b3f
vdua <vdua@adobe.com>
2024-09-23 10:55:49 +0530

    removing aem specific form block code

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,23 +1,11 @@
 import { extractFormDefinition as extractSheetDefinition, renderForm as renderDocForm } from './lib/docform.js';
+// eslint-disable-next-line no-unused-vars
 import { getSubmitBaseUrl } from './constant.js';
 
 export default async function decorate(block) {
-  let { container, formDef } = await extractSheetDefinition(block);
+  const { container, formDef } = await extractSheetDefinition(block);
   if (formDef && formDef.properties?.source === 'sheet') {
     const form = await renderDocForm(formDef);
     container.replaceWith(form);
-  } else {
-    const aemModule = await import('./lib/aemform.js');
-    const { extractFormDefinition: extractAEMDefinition, renderForm: renderAEMForm } = aemModule;
-    if (!formDef) {
-      const res = await extractAEMDefinition(block);
-      formDef = res.formDef;
-      container = res.container;
-    }
-    if (formDef) {
-      formDef.action = getSubmitBaseUrl() + (formDef.action || '');
-      const form = await renderAEMForm(formDef, block);
-      container.replaceWith(form);
-    }
   }
 }
