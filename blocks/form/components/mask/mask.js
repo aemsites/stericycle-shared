6475bc6ed5221190e54480a14fd242e092746b82
Ravisanker E <rae@ravisankers-mbp.corp.adobe.com>
2024-09-25 18:37:56 +0530

    minor correction in check

--- blocks/form/components/mask/mask.js
+++ blocks/form/components/mask/mask.js
@@ -2,7 +2,7 @@ import masking from '../../maskingUtil.js';
 
 export default function decorate(field) {
   const input = field?.querySelector('input');
-  input.dataset.type = input.name === 'zip' ? 'postalCode' : '';
+  input.dataset.type = input.type === 'tel' ? '' : 'postalCode';
   masking.init(input);
   input.addEventListener('input', (e) => masking.activateMasking(e));
   input.addEventListener('focus', () => { input.closest('.field-wrapper').classList.add('focus'); });
