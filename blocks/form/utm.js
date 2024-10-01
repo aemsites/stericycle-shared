64fdad056482d9fa414746fe58f54140a969db78
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2024-10-01 16:58:27 +0530

    moved utm source filter to placeholders

--- blocks/form/utm.js
+++ blocks/form/utm.js
@@ -1,4 +1,5 @@
-const utmSources = 'https://www.google.com/,https://www.google.ca/,https://www.youtube.com/,https://www.bing.com/,https://www.yahoo.com/,https://duckduckgo.com/,https://www.startpage.com/,https://www.qwant.com/,https://swisscows.com/,https://www.searchencrypt.com/,https://www.onesearch.com/,https://www.givewater.com/,https://www.ekoru.org/,https://www.ecosia.org/,https://unsplash.com/,https://www.dogpile.com/,https://www.excite.com/,https://www.info.com/,https://www.metacrawler.com/,https://www.ask.com/,https://www.gigablast.com/,https://metager.org/,https://www.baidu.com/,https://www.lycos.com/,https://yandex.com/,https://archive.org/,https://www.wolframalpha.com/';
+// eslint-disable-next-line import/no-unresolved
+import { fetchPlaceholders } from '../../../../../../../scripts/aem.js';
 
 const createUtmInput = (name, value, form) => {
   const input = document.createElement('input');
@@ -8,11 +9,12 @@ const createUtmInput = (name, value, form) => {
   form.appendChild(input);
 };
 
-export default function decorateUTM(form) {
+export default async function decorateUTM(form) {
+  const placeholders = await fetchPlaceholders('utm-sources');
+  const utmSources = placeholders ? Object.values(placeholders).join(',') : null;
   const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-
   utmParams.forEach((item) => {
     let utmValue = urlParams.get(item);
     const now = new Date();
@@ -31,7 +33,7 @@ export default function decorateUTM(form) {
       createUtmInput(item, utmValue, form); // Create input dynamically using cookie value
     } else if (item === 'utm_medium' || item === 'utm_source') {
       // Check if utm_medium or utm_source can be determined from the referrer
-      if (document.referrer) {
+      if (utmSources && document.referrer) {
         const url = document.referrer.match(/:\/\/(.[^/]+)/)[1];
         if (utmSources.includes(url)) {
           utmValue = item === 'utm_medium' ? 'organic' : document.referrer;
