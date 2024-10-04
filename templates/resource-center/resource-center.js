e99af9767ad35c5bbcf333ff9d0930d6b2d5b877
Piyush Jindal <jindaliiita@gmail.com>
2024-10-04 15:30:33 +0530

    Minor changes

--- templates/resource-center/resource-center.js
+++ templates/resource-center/resource-center.js
@@ -1,13 +1,11 @@
 import { decorateSidebarTemplate } from '../templates.js';
 
 export default function decorate(main) {
-  // let isSideBar = false
-  // eslint-disable-next-line no-restricted-syntax
-  for (const child of main.children) {
-    if (child.getAttribute('data-position')) {
-      main.parentElement.classList.add('with-sidebar');
-      decorateSidebarTemplate(main);
-      break;
-    }
+  const childWithPosition = Array.from(main.children)
+    .find((child) => child.getAttribute('data-position'));
+
+  if (childWithPosition) {
+    main.parentElement.classList.add('with-sidebar');
+    decorateSidebarTemplate(main);
   }
 }
