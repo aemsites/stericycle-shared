2fd804adc4d3d1a8e7fbf7b4ff6f1afd2b59e0b5
Piyush Jindal <jindaliiita@gmail.com>
2024-10-24 19:24:18 +0530

    Service location page template location details fixed (#381)
    * Service location page template location details fixed
    
    * Sorting added
    
    * Sorting Location 2 changes
    
    * Service Location CLS issue solved
    
    * Sequence changes

--- templates/service-location-page/service-location-page.js
+++ templates/service-location-page/service-location-page.js
@@ -1,15 +1,123 @@
+import usStates from '../../blocks/service-location-map/us-states.js';
 import {
   buildBlock, decorateBlock, decorateButtons, decorateIcons,
+  fetchPlaceholders,
+  getMetadata,
 } from '../../scripts/aem.js';
+import {
+  a, div, h3, h4, li, p,
+  ul,
+} from '../../scripts/dom-helpers.js';
+import { getLocale, getNearByLocations } from '../../scripts/scripts.js';
 import { decorateSidebarTemplate } from '../templates.js';
+
+const createLocDiv = async () => {
+  const ph = await fetchPlaceholders(`/${getLocale()}`);
+  const latitude = getMetadata('latitude');
+  const longitude = getMetadata('longitude');
+  let nearByLocations = [];
+
+  if (latitude && longitude) {
+    nearByLocations = await getNearByLocations({
+      latitude: parseFloat(getMetadata('latitude')),
+      longitude: parseFloat(getMetadata('longitude')),
+      name: getMetadata('name'),
+    });
+  }
+
+  const locationDiv = div({ class: 'location' });
+  const name = getMetadata('name');
+  if (name) {
+    locationDiv.append(h3(`Shred-it ${name}`));
+  }
+  const addressLine1 = getMetadata('address-line-1');
+  if (addressLine1) {
+    locationDiv.append(p(addressLine1));
+  }
+
+  const addressLine2 = getMetadata('address-line-2');
+  if (addressLine2) {
+    locationDiv.append(p(addressLine2));
+  }
+  const city = getMetadata('city');
+  if (city) {
+    locationDiv.append(p(city));
+  }
+
+  const state = getMetadata('state');
+  if (state && usStates[state]) {
+    locationDiv.append(p(usStates[state]));
+  }
+  const zipCode = getMetadata('zip-code');
+  if (zipCode) {
+    locationDiv.append(p(zipCode));
+  }
+
+  const phoneDiv = div({ class: 'phone' });
+
+  const salesNo = ph.salesno || '844-618-7651'; // fallback number also added
+  if (salesNo) {
+    phoneDiv.append(
+      div(a({ href: `tel:${salesNo}` }, salesNo), p(` - ${ph.salestext || 'Sales'}`)),
+    );
+  }
+
+  const customerNo = ph.customerserviceno || '800-697-4733'; // fallback number also added
+  if (customerNo) {
+    phoneDiv.append(
+      div(a({ href: `tel:${customerNo}` }, customerNo), p(` - ${ph.customerservicetext || 'Customer Service'}`)),
+    );
+  }
+
+  locationDiv.append(phoneDiv);
+
+  const dropOffDiv = div({ class: 'drop-off' });
+  const openingHours = getMetadata('opening-hours');
+  const dropOffInfo = getMetadata('drop-off-info');
+
+  if (openingHours || dropOffInfo) {
+    dropOffDiv.append(h4(ph.securedropofftext || 'Secure Drop-off Service'));
+
+    if (openingHours) {
+      dropOffDiv.append(p(openingHours));
+    }
+
+    if (dropOffInfo) {
+      dropOffDiv.append(p(dropOffInfo));
+    }
+
+    const zipCodeText = zipCode ? `zip=${zipCode}&` : '';
+    const url = `https://shop-shredit.stericycle.com/commerce_storefront_ui/walkin.aspx?${zipCodeText}adobe_mc=MCMID%3D47228127826121584233605487843606294434%7CMCORGID%3DFB4A583F5FEDA3EA0A495EE3%2540AdobeOrg%7CTS%3D1729746363`;
+    const buyNowAnchor = a({ class: 'button primary', href: url, target: '_blank' }, ph.buynowtext || 'Buy Now');
+    decorateButtons(buyNowAnchor);
+    dropOffDiv.append(buyNowAnchor);
+    locationDiv.append(dropOffDiv);
+  }
+
+  if (nearByLocations.length > 0) {
+    const nearByDiv = div({ class: 'nearby-locations' });
+    nearByDiv.append(h4(`${ph.nearbylocationstext || 'See our nearby locations'}:`));
+    const ulList = ul();
+    nearByLocations.forEach((loc) => {
+      const liElement = li();
+      liElement.append(a({ href: loc.path }, loc.name));
+      ulList.append(liElement);
+    });
+
+    nearByDiv.append(ulList);
+    locationDiv.append(nearByDiv);
+  }
+
+  return locationDiv;
+};
 
 /**
  * Builds service location template auto blocks and add them to the page.
  * @param {Element} main The container element
  */
-function buildServiceLocationAutoBlocks(main) {
+async function buildServiceLocationAutoBlocks(main) {
   const pageContent = main.querySelector('div.body-wrapper > div.page-content');
-  const pageSidebar = main.querySelector('div.body-wrapper > div.page-sidebar');
+  let pageSidebar = main.querySelector('div.body-wrapper > div.page-sidebar');
   const lastContentSection = pageContent.querySelector('.section:last-of-type');
 
   if (!lastContentSection) {
@@ -17,10 +125,23 @@ function buildServiceLocationAutoBlocks(main) {
   }
   // GET-A-QUOTE FORM
   const formSection = document.createElement('div');
+  formSection.style.display = 'none';
   formSection.classList.add('section');
   const form = buildBlock('get-a-quote-form', { elems: [] });
+  if (!pageSidebar) {
+    pageSidebar = document.createElement('div');
+  } else {
+    // Setting Initial set data if any because both the forms
+    // and location div are being added here only
+    // [IMPORTANT] If forms are added from the doc then we need to modify this
+    pageSidebar.innerHTML = '';
+  }
+  main.querySelector('div.body-wrapper').append(pageSidebar);
+
   formSection.prepend(form);
   pageSidebar.prepend(formSection);
+  const locDiv = await createLocDiv();
+  formSection.append(locDiv);
   decorateBlock(form);
 
   // QUOTE
@@ -87,8 +208,8 @@ function buildServiceLocationAutoBlocks(main) {
   decorateBlock(teaser);
 }
 
-export default function decorate(main) {
+export default async function decorate(main) {
   main.parentElement.classList.add('with-sidebar');
   decorateSidebarTemplate(main);
-  buildServiceLocationAutoBlocks(main);
+  await buildServiceLocationAutoBlocks(main);
 }
