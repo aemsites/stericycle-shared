8f480d6fc47557244b698683d13c5be8eb738720
diamondhandedblkman <mrosier@adobe.com>
2024-11-04 14:45:34 -0800

    adds search capabilities

---
+++ blocks/search/search.js
@@ -0,0 +1,246 @@
+import {
+  fetchPlaceholders,
+} from '../../scripts/aem.js';
+
+const searchParams = new URLSearchParams(window.location.search);
+
+export async function fetchData(source) {
+  const response = await fetch(source);
+  if (!response.ok) {
+    // eslint-disable-next-line no-console
+    console.error('error loading API response', response);
+    return null;
+  }
+
+  const json = await response.json();
+  if (!json) {
+    // eslint-disable-next-line no-console
+    console.error('empty API response', source);
+    return null;
+  }
+
+  return json.data;
+}
+
+function clearSearchResults(block) {
+  const searchResults = block.querySelector('.search-results');
+  if (searchResults) {
+    searchResults.innerHTML = '';
+  }
+}
+
+function clearSearch(block) {
+  clearSearchResults(block);
+  if (window.history.replaceState) {
+    const url = new URL(window.location.href);
+    url.search = '';
+    searchParams.delete('searchQuery');
+    window.history.replaceState({}, '', url.toString());
+  }
+}
+
+function compareFound(hit1, hit2) {
+  return hit1.minIdx - hit2.minIdx;
+}
+
+function filterData(searchTerms, data) {
+  const foundInHeader = [];
+  const foundInMeta = [];
+
+  data.forEach((result) => {
+    let minIdx = -1;
+
+    searchTerms.forEach((term) => {
+      const idx = (result.header || result.title).toLowerCase().indexOf(term);
+      if (idx < 0) return;
+      if (minIdx < idx) minIdx = idx;
+    });
+
+    if (minIdx >= 0) {
+      foundInHeader.push({ minIdx, result });
+      return;
+    }
+
+    const metaContents = `${result.title} ${result.description} ${result.path.split('/').pop()}`.toLowerCase();
+    searchTerms.forEach((term) => {
+      const idx = metaContents.indexOf(term);
+      if (idx < 0) return;
+      if (minIdx < idx) minIdx = idx;
+    });
+
+    if (minIdx >= 0) {
+      foundInMeta.push({ minIdx, result });
+    }
+  });
+
+  return [
+    ...foundInHeader.sort(compareFound),
+    ...foundInMeta.sort(compareFound),
+  ].map((item) => item.result);
+}
+
+const itemsPerPage = 10;
+let currentPage = 1;
+
+async function buildPagination(releases, ul, controls, page) {
+  const totalPages = Math.ceil(releases.length / itemsPerPage);
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const paginatedReleases = releases.slice(start, end);
+
+  ul.innerHTML = ''; // Clear previous items
+  controls.innerHTML = ''; // Clear previous controls
+  paginatedReleases.forEach((release) => {
+    const listItem = document.createElement('li');
+    const type = document.createElement('a');
+    type.href = window.location.pathname;
+    type.textContent = release['media-type'];
+    type.classList.add('type');
+    listItem.append(type);
+    const title = document.createElement('h4');
+    const titleA = document.createElement('a');
+    titleA.href = release.path;
+    titleA.textContent = release.title;
+    titleA.title = release.title;
+    title.append(titleA);
+    listItem.append(title);
+    if (Object.hasOwn(release, 'description')) {
+      const desc = document.createElement('p');
+      desc.innerText = `${release.description}`;
+      listItem.append(desc);
+    }
+    ul.appendChild(listItem);
+  });
+
+  controls.classList.add('pagination-controls');
+
+  const prev = document.createElement('li');
+  const buttonPrev = document.createElement('button');
+  buttonPrev.textContent = 'Previous';
+  buttonPrev.classList.add('fa-chevron-left');
+  buttonPrev.disabled = page === 1;
+  buttonPrev.addEventListener('click', () => {
+    currentPage -= 1;
+    buildPagination(releases, ul, controls, currentPage);
+  });
+
+  const next = document.createElement('li');
+  const buttonNext = document.createElement('button');
+  buttonNext.textContent = 'Next';
+  buttonNext.disabled = page === totalPages;
+  buttonNext.classList.add('fa-chevron-right');
+  buttonNext.addEventListener('click', () => {
+    currentPage += 1;
+    buildPagination(releases, ul, controls, currentPage);
+  });
+
+  controls.append(prev, next);
+  prev.append(buttonPrev);
+  next.append(buttonNext);
+}
+
+async function handleSearch(e, block, config) {
+  let searchValue;
+  const dispatchedEventCheck = Object.hasOwn(e, 'target') && Object.hasOwn(e.target, 'value');
+  if (dispatchedEventCheck) {
+    searchValue = e.target.value;
+  }
+  if (!dispatchedEventCheck) {
+    const tempTarget = block.querySelector('.search-input');
+    searchValue = tempTarget.value;
+  }
+  searchParams.set('searchQuery', searchValue);
+  if (window.history.replaceState) {
+    const url = new URL(window.location.href);
+    url.search = searchParams.toString();
+    window.history.replaceState({}, '', url.toString());
+  }
+
+  if (searchValue.length < 3) {
+    clearSearch(block);
+    return;
+  }
+  const searchTerms = searchValue.toLowerCase().split(/\s+/).filter((term) => !!term);
+
+  if (!Object.hasOwn(window.localStorage, 'searchIndex')) {
+    if (config.source.endsWith('.json')) {
+      window.localStorage.setItem('searchIndex', config.source);
+    }
+  }
+  const data = await fetchData(window.localStorage.getItem('searchIndex'));
+  const filteredData = filterData(searchTerms, data);
+  const placeholders = await fetchPlaceholders();
+  const source = block.querySelector('a[href]') ? block.querySelector('a[href]').href : '/query-index.json';
+  block.innerHTML = '';
+  block.append(
+    // eslint-disable-next-line no-use-before-define
+    searchBox(block, { source, placeholders }),
+  );
+
+  const prList = document.createElement('ul');
+  const pagination = document.createElement('ul');
+  await buildPagination(filteredData, prList, pagination, currentPage);
+  block.append(prList);
+  prList.insertAdjacentElement('afterend', pagination);
+  clearSearchResults(block);
+}
+
+function searchInput(block, config) {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'search');
+  input.className = 'search-input';
+
+  const searchPlaceholder = config.placeholders.searchPlaceholder || 'Search...';
+  input.placeholder = searchPlaceholder;
+  input.setAttribute('aria-label', searchPlaceholder);
+
+  input.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+      handleSearch(e, block, config);
+    }
+  });
+
+  input.addEventListener('keyup', (e) => { if (e.code === 'Escape') { clearSearch(block); } });
+  return input;
+}
+
+function searchIcon(block, config) {
+  const icon = document.createElement('span');
+  icon.classList.add('icon', 'icon-search');
+  icon.addEventListener('click', (e) => {
+    e.preventDefault();
+    const input = block.querySelector('input');
+    if (input.value) {
+      searchParams.delete('searchQuery');
+      searchParams.set('searchQuery', input.value);
+      handleSearch(e, block, config);
+    }
+  });
+  return icon;
+}
+
+function searchBox(block, config) {
+  const box = document.createElement('div');
+  box.classList.add('search-box');
+  box.append(
+    searchInput(block, config),
+    searchIcon(block, config),
+  );
+
+  return box;
+}
+
+export default async function decorate(block) {
+  const placeholders = await fetchPlaceholders();
+  const source = block.querySelector('a[href]') ? block.querySelector('a[href]').href : '/query-index.json';
+  block.innerHTML = '';
+  block.append(
+    searchBox(block, { source, placeholders }),
+  );
+
+  if (searchParams.get('searchQuery')) {
+    const input = block.querySelector('input');
+    input.value = searchParams.get('searchQuery');
+    handleSearch({}, block, { source, placeholders });
+  }
+}
