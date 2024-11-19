5efdb2f017fa9a1a72240ae39ab0d59fa1d58a1a
Bryan Stopp <bryan.stopp@gmail.com>
2024-11-18 20:38:00 -0500

    Clean up download and handle no header

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -3,22 +3,31 @@ import { getLocale } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
+  const imgIdx = Object.keys(cfg).indexOf('image');
   const ph = await fetchPlaceholders(`/${getLocale()}`);
   const { downloadnow } = ph;
 
-  const igContainer = document.createElement('div');
-  igContainer.classList.add('ig-container');
-  const headDiv = document.createElement('div');
-  const h4 = document.createElement('h4');
-  h4.textContent = cfg.title;
-  headDiv.appendChild(h4);
-  igContainer.appendChild(headDiv);
-  const downloadDiv = document.createElement('div');
+  const children = [];
+
+  if (cfg.title) {
+    const headDiv = document.createElement('div');
+    headDiv.classList.add('download-head');
+    const h4 = document.createElement('h4');
+    h4.textContent = cfg.title;
+    headDiv.appendChild(h4);
+    children.push(headDiv);
+  }
   const imageDiv = document.createElement('div');
-  const img = document.createElement('img');
-  img.src = cfg.image;
-  img.alt = cfg.title;
+  imageDiv.classList.add('download-image');
+  const picture = block.children[imgIdx].children[1].querySelector('picture');
+  const img = picture.querySelector('img');
+  const ratio = (parseInt(img.height, 10) / parseInt(img.width, 10)) * 100;
+  picture.style.paddingBottom = `${ratio}%`;
+  imageDiv.append(picture);
+  children.push(imageDiv);
+
   const ctaDiv = document.createElement('div');
+  ctaDiv.classList.add('download-cta');
   const ctaP = document.createElement('p');
   ctaP.classList.add('button-container');
   const ctaStrong = document.createElement('strong');
@@ -29,9 +38,7 @@ export default async function decorate(block) {
   ctaStrong.appendChild(ctaA);
   ctaP.appendChild(ctaStrong);
   ctaDiv.appendChild(ctaP);
-  imageDiv.appendChild(img);
-  downloadDiv.appendChild(imageDiv);
-  downloadDiv.appendChild(ctaDiv);
-  igContainer.appendChild(downloadDiv);
-  block.replaceWith(igContainer);
+  children.push(ctaDiv);
+
+  block.replaceChildren(...children);
 }
