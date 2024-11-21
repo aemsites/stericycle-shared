f70fdd35de082bbe4045052de6eb83edd178f2ba
Piyush Jindal <jindaliiita@gmail.com>
2024-11-21 12:48:33 +0530

    Hero Block changes wrt to desktop and mobile images

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -9,27 +9,46 @@ export default function decorate(block) {
   const content = wrapper.querySelector('div');
   content.classList.add('hero-content');
   contentContainer.append(content);
-  const picture = content.querySelector('picture');
-  picture.parentElement.remove();
-  picture.classList.add('hero-background');
-  let tempPic;
+  const pictures = content.querySelectorAll('picture');
 
-  if (picture.querySelector('img')) {
-    tempPic = createOptimizedPicture(
-      picture.querySelector('img')?.src,
-      'hero image',
-      true,
-      [
-        { media: '(min-width: 1600px)', width: 3000 },
-        { media: '(min-width: 600px)', width: 2500 },
-        { width: 750 },
-      ],
-    );
+  pictures.forEach((picture, index) => {
+    picture.parentElement.remove();
+    picture.classList.add('hero-background');
+    let tempPic;
 
-    tempPic.classList.add('hero-background');
-  }
+    if (picture.querySelector('img')) {
+      tempPic = createOptimizedPicture(
+        picture.querySelector('img')?.src,
+        'hero image',
+        true,
+        [
+          { media: '(min-width: 1600px)', width: 3000 },
+          { media: '(min-width: 600px)', width: 2500 },
+          { width: 750 },
+        ],
+      );
+
+      tempPic.classList.add('hero-background');
+    }
+
+    if (pictures.length === 2) {
+      if (index === 0) {
+        picture.classList.add('mobile-only');
+        tempPic?.classList.add('mobile-only');
+      } else {
+        picture.classList.add('desktop-only');
+        tempPic?.classList.add('desktop-only');
+      }
+    }
+
+    if (index === 0) {
+      block.replaceChildren(tempPic || picture);
+    } else {
+      block.append(tempPic || picture);
+    }
+  });
 
   // Add everything to the block
-  block.replaceChildren(tempPic || picture, contentContainer);
+  block.append(contentContainer);
   block.parentElement.parentElement.classList.add('blue-background');
 }
