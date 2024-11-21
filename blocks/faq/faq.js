2b767ec8e13e7d42761510f7287d87d4bfb55760
Allan Montilla <allan.montilla@herodigital.com>
2024-11-21 13:47:52 -0500

    STERI016-49: Fix issue related to the animation.

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -21,7 +21,7 @@ function addAccordionAnimation(details) {
       setTimeout(() => {
         details.removeAttribute('open');
         details.classList.remove('close');
-      }, 100); // Match this duration with the CSS animation duration
+      }, 300); // Match this duration with the CSS animation duration
     } else {
       details.classList.add('open');
       details.setAttribute('open', '');
