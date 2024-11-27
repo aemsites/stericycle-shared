f57ffe2f4be7f11d5836d439708fb9ac4f1ef9bb
conary.hernandez <conary.hernandez@herodigital.com>
2024-11-27 08:10:42 -0600

    STERI016-89: Fix linter errors

--- blocks/hero-banner/hero-banner.js
+++ blocks/hero-banner/hero-banner.js
@@ -1,43 +1,43 @@
 export default function decorate(block) {
-    const wrapper = block.querySelector("div");
+  const wrapper = block.querySelector('div');
 
-    wrapper?.classList?.add("hero-banner__content");
+  wrapper?.classList?.add('hero-banner-content');
 
-    const heroText = wrapper.querySelector("div");
-    const heroImage = heroText?.nextElementSibling;
+  const heroText = wrapper.querySelector('div');
+  const heroImage = heroText?.nextElementSibling;
 
-    heroText?.classList?.add("hero-banner__content__text");
-    heroImage?.classList?.add("hero-banner__image");
+  heroText?.classList?.add('hero-banner-content-text');
+  heroImage?.classList?.add('hero-banner-asset');
 
-    const buttons = heroText.querySelectorAll(".button-container");
+  const buttons = heroText.querySelectorAll('.button-container');
 
-    const parentDiv = document.createElement("div");
+  const parentDiv = document.createElement('div');
 
-    parentDiv?.classList?.add("hero-banner__content__ctas");
-    buttons?.forEach((button) => {
-        parentDiv?.appendChild(button);
-    });
-    heroText.appendChild(parentDiv);
+  parentDiv?.classList?.add('hero-banner-content-ctas');
+  buttons?.forEach((button) => {
+    parentDiv?.appendChild(button);
+  });
+  heroText.appendChild(parentDiv);
 
-    // Decorate content
-    const promoBadge = heroText.querySelector("h4");
-    const eyebrow = heroText.querySelector("p");
+  // Decorate content
+  const promoBadge = heroText.querySelector('h4');
+  const eyebrow = heroText.querySelector('p');
 
-    eyebrow?.classList?.add("eyebrow-small");
-    promoBadge?.classList?.add("hero-banner__promo-badge");
+  eyebrow?.classList?.add('eyebrow-small');
+  promoBadge?.classList?.add('hero-banner-promo-badge');
 
-    const isImageVariant = block.classList?.contains("hero-banner-image");
+  const isImageVariant = block.classList?.contains('hero-banner-image');
 
-    // decorate hero image
-    if (heroImage && isImageVariant) {
-        const img = heroImage.querySelector("img");
-        const picture = heroImage.querySelector("picture");
-        const imgWrapper = document.createElement("div");
+  // decorate hero image
+  if (heroImage && isImageVariant) {
+    const img = heroImage.querySelector('img');
+    const picture = heroImage.querySelector('picture');
+    const imgWrapper = document.createElement('div');
 
-        imgWrapper.classList?.add("masked-image-wrapper");
+    imgWrapper.classList?.add('masked-image-wrapper');
 
-        imgWrapper.appendChild(img);
+    imgWrapper.appendChild(img);
 
-        picture.appendChild(imgWrapper);
-    }
+    picture.appendChild(imgWrapper);
+  }
 }
