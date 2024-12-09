4489e4e78dcc3fa1ea9bf6d6fa85300b0bc15b70
Luca <adobe@gituser.de>
2024-12-09 16:54:20 +0100

    add remaining dataLayer logic

--- scripts/martech.js
+++ scripts/martech.js
@@ -6,19 +6,64 @@ function initDataLayer() {
   window.digitalData = {
     page: {
       pageInfo: {
+        pageID: window.location.pathname,
         brand: 'sid',
         pageName: getMetadata('og:title'),
         destinationURL: window.location.href,
         referringURL: document.referrer,
+        sysEnv: '', // "desktop, tablet or mobile"
+        author: '', // unavailable on EDS ?
         language: getLocaleAsBCP47(),
         pageURL: window.location.href,
-        pageType: 'homepage', // TODO: parse pagetype dynamically?
+        currentPagePath: window.location.pathname,
+        pageType: getMetadata('template') || '', // TODO: This is not a 1:1 match to the old pageTypes
         pageDescription: getMetadata('og:description'),
         country: 'US', // TODO: fetch country dynamically?
+      },
+      category: {
+        primaryCategory: '',
+        subCategory: '',
+      },
+      attributes: {
+        analytics: {
+          event: {
+            eventName: null,
+            eventAction: null,
+            eventPoints: null,
+            type: null,
+            timeStamp: new Date(),
+            effect: null,
+            category: {
+              primaryCategory: null,
+              subCategory: null,
+            },
+            attributes: {},
+          },
+        },
       },
     },
     events: [],
     version: '1.0',
+  };
+
+  window.digitalData.newEvent = (ev) => {
+    const eventContainer = {};
+    const event = {
+      event: ev?.event !== undefined ? ev.event : null,
+      formType: ev?.formType !== undefined ? ev.formType : null,
+      formName: ev?.formName !== undefined ? ev.formName : null,
+      formStep: ev?.formStep !== undefined ? ev.formStep : null,
+      searchType: ev?.searchType !== undefined ? ev.searchType : null,
+      searchTerm: ev?.searchTerm !== undefined ? ev.searchTerm : null,
+      searchResultRange: ev?.searchResultRange !== undefined ? ev.searchResultRange : null,
+      searchFilters: ev?.searchFilters !== undefined ? ev.searchFilters : null,
+      timeStamp: ev?.timeStamp !== undefined ? ev.timeStamp : new Date(),
+      quoteType: ev?.quoteType !== undefined ? ev.quoteType : null,
+      serviceType: ev?.serviceType !== undefined ? ev.serviceType : null,
+    };
+    window.digitalData.events.push(event);
+    window.digitalData.page.attributes.analytics.event = event;
+    return eventContainer;
   };
 }
 
