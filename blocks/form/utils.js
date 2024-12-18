d33a3a9fcc728790c2dd10a471d4dc29a74072db
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2024-12-18 10:12:35 +0530

    fix npe

--- blocks/form/utils.js
+++ blocks/form/utils.js
@@ -3,11 +3,12 @@ import { buildBlock, getMetadata } from '../../scripts/aem.js';
 export function getFormName(form) {
   let formName;
   if (form?.closest('.modal')) { // for form inside a modal check for this hidden field
-    formName = form.querySelector('input[name="modalFormName"]')?.value;
+    formName = form?.querySelector('input[name="modalFormName"]')?.value;
   }
   if (!formName) {
-    formName = form.querySelector('input[name="EloquaFormName"]')?.value || form.dataset.action;
+    formName = form?.querySelector('input[name="EloquaFormName"]')?.value || form?.dataset.action;
   }
+  // eslint-disable-next-line consistent-return
   return formName;
 }
 
