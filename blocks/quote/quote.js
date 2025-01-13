70e4465b575b85813778f913897154babecb6fc5
Conary <168608016+conaryh@users.noreply.github.com>
2025-01-13 07:14:22 -0600

    STERI016-233: Update quote block styles and add title support (#685)
    Co-authored-by: conary.hernandez <“conary.hernandez@herodigital.com”>

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -18,6 +18,20 @@ export default async function decorate(block) {
   }
 
   [...block.children].forEach((child) => {
+    if (child.textContent.includes('#title')) {
+      const title = document.createElement('h3');
+
+      title.className = 'heading-2';
+      title.innerHTML = child.textContent.replace('#title', '');
+      child.className = 'quote-title';
+      child.innerHTML = '';
+      child.append(title);
+
+      quoteWrapper.prepend(child);
+
+      return;
+    }
+
     const [quotation, attribution] = [...child.children].map((c) => c);
     const blockquote = document.createElement('blockquote');
 
