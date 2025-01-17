505d5ba5613d8224a3bfbc7f658d0c5d7cd3a6ae
Allan Montilla <allan.montilla@herodigital.com>
2025-01-17 17:03:15 -0500

    STERI016-87-footer-am: Remove countryData const.

--- blocks/footer/constants.js
+++ blocks/footer/constants.js
@@ -1,17 +1,3 @@
-// Data for countries
-export const countryData = [
-  { name: 'United States', url: 'https://www.shredit.com/en-us' },
-  { name: 'Canada', url: 'https://www.shredit.com/en-ca' },
-  { name: 'Belgium', url: 'https://www.shredit.be/nl-be' },
-  { name: 'Germany', url: 'https://www.shredit.de/de-de' },
-  { name: 'Spain', url: 'https://www.shredit.es/es-es' },
-  { name: 'France', url: 'https://www.shredit.fr/fr-fr' },
-  { name: 'United Kingdom', url: 'https://www.shredit.co.uk/en-gb' },
-  { name: 'Ireland', url: 'https://www.shredit.ie/en-ie' },
-  { name: 'Luxembourg', url: 'https://www.shredit.lu/fr-lu' },
-  { name: 'Netherlands', url: 'https://www.shredit.nl/nl-nl' },
-  { name: 'Portugal', url: 'https://www.shredit.pt/pt-pt' },
-];
-
 // media query match that indicates mobile/tablet width
+// eslint-disable-next-line import/prefer-default-export
 export const isDesktop = window.matchMedia('(min-width: 1200px)');
