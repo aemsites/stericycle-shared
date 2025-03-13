d13f3ab1c51f1dd6d8f70a1fcc49983d9e842baa
Conary <168608016+conaryh@users.noreply.github.com>
2025-03-13 10:39:40 -0600

    STERI016-484: Update icon logic for flipcard (#865)
    * STERI016-484: Update icon logic for flipcard
    
    * STERI016-484: Update icon logic for flipcard
    
    ---------
    
    Co-authored-by: conary.hernandez <“conary.hernandez@herodigital.com”>

--- blocks/flip-cards/flip-cards.js
+++ blocks/flip-cards/flip-cards.js
@@ -1,5 +1,5 @@
 import { decorateButtons } from '../../scripts/aem.js';
-import { a, h3, p, span } from '../../scripts/dom-helpers.js';
+import { a, h3, p } from '../../scripts/dom-helpers.js';
 import { fetchQueryIndex } from '../../scripts/scripts.js';
 import { adjustHeightsOnResize } from './utils.js';
 
@@ -60,19 +60,12 @@ export default async function decorate(block) {
 
       const url = page.href;
 
-      const existingSpan = page.querySelector('.icon');
-      const spanElement = existingSpan
-        ? span({
-          class: existingSpan.classList.value,
-          'data-icon-src': existingSpan.querySelector('img')?.getAttribute('src') || '/icons/right-arrow.svg',
-          style: `--mask-image: url(${existingSpan.querySelector('img')?.getAttribute('src') || '/icons/right-arrow.svg'});`,
-        })
-        : '';
+      const existingSpan = page.querySelector('.icon') || '';
 
       const readMoreButton = a(
         { class: page.classList.value || 'button primary', href: url, target: '_self' },
         page.title || 'Read More',
-        spanElement,
+        existingSpan,
       );
 
       decorateButtons(readMoreButton);
@@ -110,12 +103,13 @@ export default async function decorate(block) {
           desc.appendChild(document.createTextNode(para.textContent));
         });
         desc.classList.add('clamp-description');
+        const existingSpan = page.querySelector('.icon') || '';
 
         const url = page.href;
         const readMoreButton = a(
           { class: page.classList.value || 'button primary', href: url, target: '_self' },
           page.title || 'Read More',
-          span({ class: 'icon icon-right-arrow', 'data-icon-src': '/icons/right-arrow.svg', style: '--mask-image: url(/icons/right-arrow.svg);' }),
+          existingSpan,
         );
         decorateButtons(readMoreButton);
         const pButton = document.createElement('p');
