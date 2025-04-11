547c32a882e26742c38d3901c35faac018847dc8
Eric VanGeem <eric.vangeem@herodigital.com>
2025-04-11 16:12:28 -0500

    refactor getRelatedPosts for DA indexes.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -160,9 +160,9 @@ export const haversineDistance = (lat1, lon1, lat2, lon2) => {
  * @param {string} [locale] sheet locale (uses current page locale none is supplied)
  * @returns {*} fetched query index
  */
-export function fetchQueryIndex(locale) {
+export function fetchQueryIndex(locale, sheet = 'query') {
   const sheetLocale = locale || getLocale();
-  return ffetch(`/${sheetLocale}/query-index.json`);
+  return ffetch(`/${sheetLocale}/${sheet}-index.json`);
 }
 
 // Remove dedup based on name, specific to the locations
@@ -245,12 +245,10 @@ export async function getRelatedPosts(types, tags, limit) {
 
   // fetch all posts by type
   let posts = [];
-  const fetchResults = await Promise.all(sheets.map(async (sheet) => fetchQueryIndex().sheet(sheet).all()));
+  const fetchResults = await Promise.all(sheets.map(async (sheet) => fetchQueryIndex(null, sheet).all()));
   fetchResults.forEach((fetchResult) => posts.push(...fetchResult));
-  if (nTypes.length > 1) {
-    // this could become a performance problem with a huge volume of posts
-    posts = posts.sort((a, b) => b.date - a.date);
-  }
+  // this could become a performance problem with a huge volume of posts
+  posts = posts.sort((a, b) => b.date - a.date);
 
   // filter posts by tags
   const filteredPosts = [];
