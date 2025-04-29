36494170c73f8db2394630d00da54685282f1e97
conary.hernandez <conary.hernandez@herodigital.com>
2025-04-29 17:29:52 -0600

    STERICMS-862: Add margin to icon list items and handle odd last child styling

--- blocks/icon-list/icon-list.js
+++ blocks/icon-list/icon-list.js
@@ -1,4 +1,11 @@
 /* eslint-disable no-unused-vars */
 export default function decorate(block) {
-  // not needed for this block
+  const iconList = block.querySelectorAll(
+    '.icon-list.block.two-column-icons > div',
+  );
+  const lastChild = iconList[iconList.length - 1];
+
+  if (iconList && iconList.length % 2 !== 0) {
+    lastChild.classList.add('odd-last-child');
+  }
 }
