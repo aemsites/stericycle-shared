60810c25cf72ae7a66a904ff522afa14b7aeedfc
conary.hernandez <conary.hernandez@herodigital.com>
2025-05-08 10:59:49 -0600

    STERICMS-862: Add odd-last-child class to the last item in icon list for better styling

--- blocks/icon-list/icon-list.js
+++ blocks/icon-list/icon-list.js
@@ -3,9 +3,10 @@ export default function decorate(block) {
   const iconList = block.querySelectorAll(
     '.icon-list.block.two-column-icons > div',
   );
-  const lastChild = iconList[iconList.length - 1];
 
   if (iconList && iconList.length % 2 !== 0) {
+    const lastChild = iconList[iconList.length - 1];
+
     lastChild.classList.add('odd-last-child');
   }
 }
