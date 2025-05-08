1b3199eb8827f9c64e1dddcbc99dab90f228c5db
conary.hernandez <conary.hernandez@herodigital.com>
2025-05-08 11:20:18 -0600

    STERICMS-862: Remove unnecessary odd-last-child logic from icon list block

--- blocks/icon-list/icon-list.js
+++ blocks/icon-list/icon-list.js
@@ -1,12 +1,4 @@
 /* eslint-disable no-unused-vars */
 export default function decorate(block) {
-  const iconList = block.querySelectorAll(
-    '.icon-list.block.two-column-icons > div',
-  );
-
-  if (iconList && iconList.length % 2 !== 0) {
-    const lastChild = iconList[iconList.length - 1];
-
-    lastChild.classList.add('odd-last-child');
-  }
+  // not needed for this block
 }
