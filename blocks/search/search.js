0164a577b9949d08f251632ee18d00cdde128810
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2025-06-18 14:19:19 -0400

    filter search results by allowed media types and sort by date

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -238,7 +238,12 @@ async function handleSearch(e, block, config) {
   }
 
   if (searchValue.length >= 3) {
-    const data = await fetchQueryIndex().sheet('search').all();
+    const allowedTypes = ['Blogs', 'Service Location', 'Press Releases', 'Service-page', 'Industry-page', 'Videos', 'Info Sheets'];
+    const data = await fetchQueryIndex()
+      .filter((post) => allowedTypes.map(type => type.toLowerCase()).includes(post['media-type'].toLowerCase()))
+      .all();
+
+    data.sort((a, b) => b.date - a.date);
 
     const searchTerms = searchValue.toLowerCase().split(/\s+/).filter((term) => !!term);
     const filteredData = filterData(searchTerms, data);
