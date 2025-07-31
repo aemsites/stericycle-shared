86e1e420f8028f05073d9b5d05abdf65fa98f6a0
Ann Chapman <ann.chapman@avionos.com>
2025-07-31 15:55:03 -0500

    dynamically pass country and lang in :currentPagePath form field

--- blocks/form/submit.js
+++ blocks/form/submit.js
@@ -133,7 +133,7 @@ async function constructPayload(form, captcha) {
   const [language, country] = getCountryAndLanguage();
   const payload = {
     __id__: generateUnique(),
-    ':currentPagePath': '/content/shred-it/us/en',
+    ':currentPagePath': `/content/shred-it/${country}/${language}`,
     currentPagePath: window.location.pathname,
     jobPropertiesUrl: `https://main--shredit--stericycle.aem.page${form.dataset.action}.json`,
     formName: form.dataset?.action,
