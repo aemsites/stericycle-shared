2efc726c24d87f45fafe292043747b0192430e24
Gabriel Martinez <gabriel.martinez@hugeinc.com>
2025-08-05 12:33:37 -0500

    Refactor about importer and the template is using

--- tools/importer/about.js
+++ tools/importer/about.js
@@ -11,12 +11,10 @@
  */
 /* global WebImporter */
 /* eslint-disable no-console, class-methods-use-this */
-
-const baseDomain = 'https://main--shred-it--stericycle.aem.page';
 const req = new XMLHttpRequest();
 let tags = {};
 const TAGS = {};
-req.open('GET', '/tools/importer/metadata/en-us-shredit-meta.json', false);
+req.open('GET', '/tools/importer/metadata/en-ca-shredit-meta.json', false);
 req.send(null);
 if (req.status === 200) {
   tags = JSON.parse(req.responseText);
@@ -24,25 +22,13 @@ if (req.status === 200) {
 
 function getPath(url) {
   const lastIndex = url.lastIndexOf('/');
-  const path = url.substring(lastIndex + 1);
-  return path;
+  return url.substring(lastIndex + 1);
 }
 
 tags.forEach((item) => {
   const path = getPath(item.Path);
   TAGS[path] = item.Tags.replaceAll(';', ',');
 });
-
-function getLocaleFromUrl(doc) {
-  const match = doc.documentURI.match(/\/([a-z]{2,}(?:-[a-z]{2,})*)\//g);
-  return Object.hasOwn(match, 'length') && match.length >= 1 ? match[0].replaceAll('/', '') : null;
-}
-
-function sanitizeURL(url) {
-  const newURL = baseDomain + url;
-
-  return newURL;
-}
 
 function getDocumentMetadata(name, document) {
   const attr = name && name.includes(':') ? 'property' : 'name';
@@ -51,6 +37,72 @@ function getDocumentMetadata(name, document) {
     .join(', ');
   return meta || '';
 }
+
+function getLocaleFromUrl(doc) {
+  const match = doc.documentURI.match(/\/([a-z]{2,}(?:-[a-z]{2,})*)\//g);
+  return Object.hasOwn(match, 'length') && match.length >= 1 ? match[0].replaceAll('/', '') : null;
+}
+
+function setMetadata(meta, document) {
+  const url = new URL(document.documentURI).pathname;
+  const path = getPath(url);
+  const pubDate = document.querySelector('.cmp-calendarattributeprojection');
+  meta.template = 'pr-page';
+  meta['media-type'] = 'Press Releases'; // all pages should have a value for this
+  meta['publication-date'] = pubDate.innerText;
+  meta['twitter:title'] = meta.Title;
+  meta['twitter:description'] = meta.Description;
+  meta['og:type'] = 'website';
+  meta.locale = getLocaleFromUrl(document);
+  meta['og:url'] = getDocumentMetadata('og:url', document);
+  if (Object.hasOwn(TAGS, path) && TAGS[path] !== '') {
+    meta.tags = TAGS[path];
+  }
+}
+
+function fixDynamicMedia(main, document) {
+  const dmImages = main.querySelectorAll('div[data-cmp-is="image"]');
+  dmImages.forEach((img) => {
+    const source = img.getAttribute('data-cmp-src').replace('&width={width}', '');
+    const newImg = document.createElement('img');
+    newImg.src = source;
+    img.append(newImg);
+  });
+}
+
+function decodeAndReplace(str) {
+  const replacements = {
+    '%C3%A1': 'a',
+    '%C3%A9': 'e',
+    '%C3%AD': 'i',
+    '%C3%B3': 'o',
+    '%C3%BA': 'u',
+    '%C3%B1': 'n',
+  };
+  let updatedStr = str;
+  Object.entries(replacements).forEach(([encoded, plain]) => {
+    const regex = new RegExp(encoded, 'g');
+    updatedStr = updatedStr.replace(regex, plain);
+  });
+  return updatedStr;
+}
+
+/**
+ * Updates the given URL by replacing consecutive hyphens in the pathname with a single hyphen,
+ * and removes a trailing hyphen from the pathname if present.
+ *
+ * @param {string} originalURL - The original URL to be updated.
+ * @returns {string} The updated, valid URL as a string.
+ */
+function updatetoValidUrl(originalURL) {
+  const url = new URL(originalURL);
+  url.pathname = url.pathname.replace(/--+/g, '-');
+  url.pathname = url.pathname.endsWith('-')
+    ? url.pathname.slice(0, -1)
+    : url.pathname;
+  return decodeAndReplace(url.toString());
+}
+
 
 function transformButtonToAnchors(main) {
   let aLink = main.querySelector('a.page-teaser__link--wrapper').href;
@@ -153,56 +205,6 @@ function transformCards(main) {
   });
 }
 
-function setMetadata(meta, document) {
-  const url = new URL(document.documentURI).pathname;
-  const path = getPath(url);
-  const pubDate = document.querySelector('.cmp-calendarattributeprojection');
-  meta.template = 'pr-page';
-  meta['media-type'] = 'Press Releases'; // all pages should have a value for this
-  meta['publication-date'] = pubDate.innerText;
-  meta['twitter:title'] = meta.Title;
-  meta['twitter:description'] = meta.Description;
-  meta['og:type'] = 'website';
-  meta.locale = getLocaleFromUrl(document);
-  meta['og:url'] = getDocumentMetadata('og:url', document);
-  if (Object.hasOwn(TAGS, path) && TAGS[path] !== '') {
-    meta.tags = TAGS[path];
-  }
-}
-
-function decodeAndReplace(str) {
-  const replacements = {
-    '%C3%A1': 'a',
-    '%C3%A9': 'e',
-    '%C3%AD': 'i',
-    '%C3%B3': 'o',
-    '%C3%BA': 'u',
-    '%C3%B1': 'n',
-  };
-  let updatedStr = str;
-  Object.entries(replacements).forEach(([encoded, plain]) => {
-    const regex = new RegExp(encoded, 'g');
-    updatedStr = updatedStr.replace(regex, plain);
-  });
-  return updatedStr;
-}
-
-
-/**
- * Updates the given URL by replacing consecutive hyphens in the pathname with a single hyphen,
- * and removes a trailing hyphen from the pathname if present.
- *
- * @param {string} originalURL - The original URL to be updated.
- * @returns {string} The updated, valid URL as a string.
- */
-function updatetoValidUrl(originalURL) {
-  const url = new URL(originalURL);
-  url.pathname = url.pathname.replace(/--+/g, '-');
-  url.pathname = url.pathname.endsWith('-')
-    ? url.pathname.slice(0, -1)
-    : url.pathname;
-  return decodeAndReplace(url.toString());
-}
 
 export default {
   /**
@@ -221,6 +223,12 @@ export default {
     // define the main element: the one that will be transformed to Markdown
     const main = document.body;
 
+    WebImporter.DOMUtils.remove(document, [
+      'script[src*="https://solutions.invocacdn.com/js/invoca-latest.min.js"]',
+    ]);
+
+    fixDynamicMedia(main, document);
+
     // attempt to remove non-content elements
     WebImporter.DOMUtils.remove(main, [
       'header',
@@ -236,8 +244,6 @@ export default {
       'div.cmp-experiencefragment--footer',
       'div.col-lg-3.cmp-columnrow__item',
       'div#onetrust-consent-sdk',
-      'div.related-content',
-      '.text.ss-font-color--white',
     ]);
 
     /*
@@ -253,10 +259,10 @@ export default {
 
     const mdb = WebImporter.Blocks.getMetadataBlock(document, meta);
     main.append(mdb);
+
     WebImporter.rules.transformBackgroundImages(main, document);
     WebImporter.rules.adjustImageUrls(main, params.originalURL, params.originalURL);
     WebImporter.rules.convertIcons(main, document);
-
     return main;
   },
 
@@ -273,7 +279,7 @@ export default {
     // eslint-disable-next-line no-unused-vars
     document, url, html, params,
   }) => {
-    url  = updatetoValidUrl(params.originalURL);
+    url = updatetoValidUrl(url);
     let p = new URL(url).pathname;
     if (p.endsWith('/')) {
       p = `${p}index`;
