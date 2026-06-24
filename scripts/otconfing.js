f9ea318b83f51541079cfa47164d3fc3da7553ef
Mauricio Ochoa <mauricio.ochoa@hugeinc.com>
2026-06-24 15:48:23 -0500

    STERICMS-959 Refactor OneTrust configuration for improved clarity and maintainability

--- scripts/otconfing.js
+++ scripts/otconfing.js
@@ -1,22 +1,18 @@
 import { loadScript } from './aem.js';
 
-export default function getOneTrustConfig(pageUrl) {
-  const otConfigMap = [
-    {
-      pattern: /^https?:\/\/(www\.)?shredit\.com(\/|$)/,
-      domainScript: '94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c',
-      script: 'https://cdn.cookielaw.org/consent/94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c/OtAutoBlock.js',
-    },
-    {
-      pattern: /^https?:\/\/dev-us\.shredit\.com(\/|$)/,
-      domainScript: '94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c-test',
-      script: 'https://cdn.cookielaw.org/consent/94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c-test/OtAutoBlock.js',
-    },
-  ];
+const PROD_CONFIG = {
+  domainScript: '94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c',
+  script: 'https://cdn.cookielaw.org/consent/94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c/OtAutoBlock.js',
+};
 
-  const matched = otConfigMap.find((config) => config.pattern.test(pageUrl));
-  if (matched) return matched;
-  return otConfigMap[0]; // default config
+const TEST_CONFIG = {
+  domainScript: '94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c-test',
+  script: 'https://cdn.cookielaw.org/consent/94a9f9f7-2ccd-4f46-b1a1-d11d479ed08c-test/OtAutoBlock.js',
+};
+
+export default function getOneTrustConfig(pageUrl) {
+  if (/^https?:\/\/(www\.)?shredit\.com(\/|$)/.test(pageUrl)) return PROD_CONFIG;
+  return TEST_CONFIG;
 }
 
 async function addCookieBanner() {
